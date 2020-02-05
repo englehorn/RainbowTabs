6a0ee3715f55a2b7545425faabbfd881ff456a72
Andrew Englehorn <>
2020-02-05 13:01:58 -0800

    first commit

---
+++ RainbowTabs/RedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by AE on 1/25/20.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarItem.badgeValue = "!"
+        
+    }
+
+
+}
+
